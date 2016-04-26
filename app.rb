fd164603a5612209be05125f736b718b278fc267
Amy Nicholson <missamynicholson@gmail.com>
2016-04-26 16:46:46 +0100

    Challenge 19

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     p params
-    @Name_1 = params[:Name_1]
-    @Name_2 = params[:Name_2]
+    session[:Name_1] = params[:Name_1]
+    session[:Name_2] = params[:Name_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Name_1 = session[:Name_1]
+    @Name_2 = session[:Name_2]
     erb(:play)
   end
 
