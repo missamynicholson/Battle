5bb882f3b4fe60cec7e63396a61a304326353964
Ben Stein <bumstain@gmail.com>
2016-04-27 23:06:49 +1000

    Challenge 22: Players can attack each other

---
+++ spec/features/attack_player_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Attack other player' do
+  scenario "Player 1 can attack Player 2" do
+    sign_in_and_play
+    attack
+    expect(page).to have_content("Ben attacked Chris!")
+  end
+end 
