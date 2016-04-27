68f0b6ea2ed8396d4cfd878a2805998a6bfcffd3
letianw <tmac4ever0824@gmail.com>
2016-04-27 22:59:20 +1000

    Closing # 19

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-  	@name1 = params[:Name_1]
-  	@name2 = params[:Name_2]
-  	erb(:play)
+  	session[:Name_1] = params[:Name_1]
+  	session[:Name_2] = params[:Name_2]
+  	redirect('/play')
+  end
+
+  get '/play' do
+    @name1 = session[:Name_1]
+    @name2 = session[:Name_2]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
