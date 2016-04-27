165dce0c652f176c240df042b7dfe8a61174b7f2
chriselevate <coates.c@hotmail.com>
2016-04-27 13:44:04 +0100

    21_test_helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Enter names' do
 	scenario "Players submit their names" do
-		visit('/')
-		fill_in('name1', with:"Ben")
-		fill_in('name2', with:"Chris")
-		click_button('Submit')
+    sign_in_and_play
 		expect(page).to have_text("Hey Ben, hey Chris! Welcome to the Battle...")
 	end
 end
