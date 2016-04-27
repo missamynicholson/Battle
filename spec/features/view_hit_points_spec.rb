6bc47141a19ed444913ebeb0cdef7390dcd7768a
Amy Nicholson <missamynicholson@gmail.com>
2016-04-27 09:59:41 +0100

    Updating master - Challenge 21

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper.rb'
 
 feature "view hit points" do
   scenario "player 1 can view player 2's hit points" do
-    visit "/"
-    fill_in "name_1", :with => "Player_1"
-    fill_in "name_2", :with => "Player_2"
-    click_button "Start"
+  sign_in_and_play
     expect(page).to have_content("Player_2: 50HP")
   end
 end
