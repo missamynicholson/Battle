8927ec31fa04d3f4bf673b02721ea67ea08292a6
Amy Nicholson <missamynicholson@gmail.com>
2016-04-28 20:15:38 +0100

    [WIP] Challenge 29

---
+++ spec/features/lose_confirmation_spec.rb
@@ -0,0 +1,8 @@
+feature "Lose confirmation message when hitpoints get to 0HP" do
+  scenario "Lose message if Player 2's hps reach 0HP first" do
+    sign_in_and_play
+    8.times {attack_and_switch}
+    click_link('Attack')
+    expect(page).to have_content("Sorry Player_2, you've lost.")
+  end
+end
