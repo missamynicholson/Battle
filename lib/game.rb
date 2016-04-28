34e56a0d9631c857374d50844fe123839963de60
Amy Nicholson <missamynicholson@gmail.com>
2016-04-28 21:05:17 +0100

    Challenge 29

--- lib/game.rb
+++ lib/game.rb
@@ -13,4 +13,12 @@ class Game
   def switch_roles
     @players[0], @players[1] = @players[1], @players[0]
   end
+
+  def game_over?
+    players[1].hitpoints == 0 || players[0].hitpoints == 0
+  end
+
+  def loser
+    players.each {|player| return player if player.hitpoints == 0}
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,10 @@ require 'game'
 
 describe Game do
   subject(:game){described_class.new(player_1: player_1, player_2: player_2)}
-  let(:player_1) {double :player_1}
-  let(:player_2) {double :player_2}
+  let(:player_1) {double :player_1, hitpoints: 50}
+  let(:player_2) {double :player_2, hitpoints: 50}
+  let(:player_lost) {double :player_lost, hitpoints: 0}
+  let(:game_over){described_class.new(player_1: player_1, player_2: player_lost)}
 
   describe '#player_1' do
     it 'retrieves the first player' do
@@ -22,6 +24,20 @@ describe Game do
     it 'switches the role of player_1 and player_2' do
       game.switch_roles
       expect(game.players[0]).to eq player_2
+    end
+  end
+
+  describe '#game over' do
+    it 'game over if one player has 0 HPs' do
+      expect(game_over.game_over?).to eq true
+    end
+
+    it 'game not over if none of players have 0 HPs' do
+      expect(game.game_over?).to eq false
+    end
+
+    it 'returns name of loser' do
+      expect(game_over.loser).to eq player_lost
     end
   end
 end
