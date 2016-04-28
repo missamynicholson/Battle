7a63776e0b69d98b28edfa2e1dc2da60aaf4cffb
chriselevate <coates.c@hotmail.com>
2016-04-28 12:27:17 +0100

    26_skinny_controllers - complete

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,20 @@ require 'game'
 
 describe Game do
 
-  subject(:game)  { described_class.new }
-  let(:chris)     { double :player }
-  let(:ben)       { double :player }
+  subject(:game)  { described_class.new(player1, player2) }
+  let(:player1)   { double :player }
+  let(:player2)   { double :player }
 
+  describe 'player 1' do
+    it "gets player 1" do
+      expect(game.player1).to eq player1
+    end
+  end
 
   describe '#attack' do
     it "damages the other player" do
-      expect(chris).to receive(:lose_hit_points)
-      game.attack(chris)
+      expect(player1).to receive(:lose_hit_points)
+      game.attack(player1)
     end
   end
 end
